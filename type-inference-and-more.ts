0e36ab26bb58a1131294077fc8efa684aff1165d
luchang666666 <1342018842@qq.com>
2020-12-21 23:16:56 +0800

    提交类型断言ts文件

---
+++ type-inference-and-more.ts
@@ -0,0 +1,22 @@
+let str = 'str'
+
+//联合类型 可以指定变量为多个类型
+let numberOrString :number|string
+
+function getLength(input: string|number): number {
+    const str = input as string             //as 类型断言 告诉编译器我要把它作为string使用
+    if(str.length){
+        return str.length
+    }else {
+        const number = input as number
+        return number.toString().length
+    }
+}
+//type guard 类型守护 自动缩小类型断言范围
+function getLength2(input: string|number): number {
+    if(typeof input==="string"){
+        return input.length
+    }else {     //else中自动变成number类型
+        return input.toString().length
+    }
+}
