ec798cad6c946d4e8022e439adc127cfb41423e8
一个有理想的苦逼程序员 <1342018842@qq.com>
2020-12-21 17:17:27 +0800

    '新增一些typescript基础语法'

---
+++ interface-basic.js
@@ -0,0 +1,5 @@
+var luchang = {
+    id: 1,
+    name: 'luchang'
+};
+// luchang.id=2                 id为只读属性，无法赋值
---
+++ typescript-basic.js
@@ -0,0 +1,10 @@
+var isFlag = true; //布尔类型
+var first = 'luchang'; //字符串类型
+var FirstName = "first " + name; //字符串加变量
+var u = undefined; //未定义类型：只能赋值为undefined
+var n = null; //空类型 :只能赋值为null
+var num = 111; //number类型
+var a = 4; //any任意类型，和var类似 可以赋值任意类型而且不会报错 同样取任何属性也不会报错
+a = '1232312312';
+a.aaa = 111;
+a.set();
